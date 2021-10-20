8cfe636417984ca62a86a2a08d0132aea68904bb
ViktorijaSiktorija <51799486+ViktorijaSiktorija@users.noreply.github.com>
2021-10-20 15:07:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require ("express");
 const https = require('https');
 const bodyParser = require('body-parser')
 
-
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,9 +37,6 @@ app.post("/", (req,res) => {
     });
   });
 });
-
-
-
 
 app.listen(3000, () => {
   console.log("port on 3000");
